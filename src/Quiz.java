8fb88005b1ebbfe4271483b4e1a7ca037b96dfb7
bijanghanei <iambijanghanei@gmail.com>
2023-11-16 19:11:50 -0500

    Cleaned quiz file to start

--- src/Quiz.java
+++ src/Quiz.java
@@ -5,14 +5,11 @@ import java.awt.event.ActionListener;
 public class Quiz implements ActionListener {
     QuizStyle quizStyle = new QuizStyle();
     Quiz(){
-        quizStyle.frame.setTitle("TESSSSSSSSSSSSSSSSST");
-        quizStyle.buttonA.addActionListener(this);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == quizStyle.buttonA){
-            System.out.println("Boooooooooooooooooooo");
-        }
+
     }
 }
