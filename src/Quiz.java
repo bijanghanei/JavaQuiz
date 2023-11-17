1d80c27a5639174f4ca59c80b6cfa125bb3215ca
bijanghanei <iambijanghanei@gmail.com>
2023-11-17 00:36:59 -0500

    Refactored some parts and added functionality

--- src/Quiz.java
+++ src/Quiz.java
@@ -5,61 +5,94 @@ import java.awt.event.ActionListener;
 
 public class Quiz implements ActionListener {
     QuizStyle quizStyle = new QuizStyle();
-    JButton[] buttons = {quizStyle.buttonA,quizStyle.buttonB,quizStyle.buttonC,quizStyle.buttonD};
-    JLabel[] labels = {quizStyle.answerLabelA,quizStyle.answerLabelB,quizStyle.answerLabelC,quizStyle.answerLabelD};
+    JButton[] buttons = {quizStyle.buttonA, quizStyle.buttonB, quizStyle.buttonC, quizStyle.buttonD};
+    JLabel[] labels = {quizStyle.answerLabelA, quizStyle.answerLabelB, quizStyle.answerLabelC, quizStyle.answerLabelD};
 
-    String[] questions =    {
-                                "What is the capital of Canada?",
-                                "What is capital of USA?",
-                                "How many countries are there?",
-                                "what is the SECOND most spoken language in the world?"
-                            };
-    String[][] options =    {
-                                {"Ottawa", "Toronto", "Vancouver", "London"},
-                                {"New York", "Washington DC", "California", "Los Angeles"},
-                                {"156", "329", "195", "97"},
-                                {"English", "Chinese", "Spanish", "Hindi"},
-                            };
-    String[] answers =    {
-                            "A","B","C","D"
-                        };
+    String[] questions = {
+            "What is the capital of Canada?",
+            "What is capital of USA?",
+            "How many countries are there?",
+            "what is the SECOND most spoken language in the world?"
+    };
+    String[][] options = {
+            {"Ottawa", "Toronto", "Vancouver", "London"},
+            {"New York", "Washington DC", "California", "Los Angeles"},
+            {"156", "329", "195", "97"},
+            {"English", "Chinese", "Spanish", "Hindi"},
+    };
+    String[] answers = {
+            "A", "B", "C", "C"
+    };
     int correctAnswers = 0;
     int index = 0;
     int timeCounter = 10;
     String answered = "";
-    Quiz(){
+
+    Quiz() {
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].addActionListener(this);
+        }
         setupQuestion(index);
     }
-    private void setupQuestion(int number){
-        quizStyle.textField.setText("Question"+(number+1));
+
+    private void setupQuestion(int number) {
+        quizStyle.textField.setText("Question" + (number + 1));
         quizStyle.textArea.setText(questions[number]);
-        quizStyle.answerLabelA.setText(options[number][0]);
-        quizStyle.answerLabelB.setText(options[number][1]);
-        quizStyle.answerLabelC.setText(options[number][2]);
-        quizStyle.answerLabelD.setText(options[number][3]);
-        timeCounter = 10;
+        for (int i = 0; i < labels.length; i++) {
+            labels[i].setForeground(Color.white);
+            labels[i].setText(options[number][i]);
+        }
     }
-    private void displayResult(){
+
+    private void displayResult() {
 
     }
-    private void displayAnswer(int number){
-        quizStyle.buttonA.setEnabled(false);
-        quizStyle.buttonB.setEnabled(false);
-        quizStyle.buttonC.setEnabled(false);
-        quizStyle.buttonD.setEnabled(false);
-        for (int i = 0; i<buttons.length ; i++){
-            if (buttons[i].getText() == answers[number]){
+
+    private void displayAnswer(int number) {
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setEnabled(false);
+            if (buttons[i].getText() == answers[number]) {
                 labels[i].setForeground(Color.green);
-            }else{
+            } else {
                 labels[i].setForeground(Color.red);
             }
         }
+        Timer pause = new Timer(2000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (index<questions.length-1){
+                    index++;
+                }
+                for (int i = 0; i < buttons.length; i++) {
+                    buttons[i].setEnabled(true);
+                    buttons[i].setBackground(new Color(25,25,25));
+                    labels[i].setForeground(Color.white);
+
+                }
+                answered = "";
+                timeCounter = 10;
+                setupQuestion(index);
+            }
+        });
+        pause.setRepeats(false);
+        pause.start();;
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == quizStyle.buttonA){
-            answered = "A";
-        }
+        for (int i = 0; i < buttons.length; i++) {
+            if (e.getSource() == buttons[i]) {
+                answered = buttons[i].getText();
+                if (answered == answers[index]) {
+                    buttons[i].setBackground(Color.green);
+                    correctAnswers++;
+                } else {
+                    buttons[i].setBackground(Color.red);
+                }
+                displayAnswer(index);
+            }
 
+
+        }
     }
 }
